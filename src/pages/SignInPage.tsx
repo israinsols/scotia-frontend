d5112fb4b8a29b852ae2bbe1bf5d75f2ea4d9cfb
Asad Hussain <asadhussain5768@gmail.com>
2026-09-26 17:07:14 +0500

    Update Lock icon size in SignInPage component

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -71,8 +71,8 @@ export const SignInPage: React.FC<SignInPageProps> = ({ onNavigateHome }) => {
           
           {/* Lock Icon in Top Right of Card */}
           <div className="absolute top-6 right-6">
-            <div className="w-5 h-5 border border-gray-400/80 rounded flex items-center justify-center bg-white">
-              <Lock className="w-3 h-3 text-gray-700 stroke-[2.2]" />
+            <div className="w-5 h-5 rounded flex items-center justify-center bg-white">
+              <Lock className="w-5 h-5 text-gray-700 stroke-[2.2]" />
             </div>
           </div>
 
